25047a4ac6c845c97646188ce5e2893e51fa6770
Garrin Shieh <garrin.shieh@gmail.com>
2021-03-31 17:21:56 -0400

    updated DequeIterator

--- TheManifesto/src/DequeIterator.java
+++ TheManifesto/src/DequeIterator.java
@@ -1,23 +1,24 @@
 class DequeIterator implements Iterator<T> {
   Deque deque;
-  Index x;
+  int x;
+  private Deque this.deque.find(x);
 
   DequeIterator(Deque deque) {
     this.deque = deque;
-    this.x = x;
+    this.x = deque.size() - 1;
   }
 
   boolean hasNext() {
-    return this.deque.size() >= index;
+    return x < 0;
   }
 
   T next() {
-    if(!this.hasNext()) {
+    if (!this.hasNext()) {
       throw new NoSuchElementException();
     }
 
-    T answer = this.deque;
-    return T;
+    T answer = this.deque.find(x);
     x++;
+    return answer
   }
 }
